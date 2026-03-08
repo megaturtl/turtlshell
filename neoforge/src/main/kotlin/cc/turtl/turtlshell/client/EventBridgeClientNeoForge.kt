dcae1a13930089e076ae54cff82e0dceeed6f7d4
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2026-03-08 22:15:40 +1100

    Update EventBridgeClientNeoForge.kt

--- neoforge/src/main/kotlin/cc/turtl/turtlshell/client/EventBridgeClientNeoForge.kt
+++ neoforge/src/main/kotlin/cc/turtl/turtlshell/client/EventBridgeClientNeoForge.kt
@@ -16,46 +16,46 @@ object EventBridgeClientNeoForge {
 
     @SubscribeEvent
     fun onClientTick(e: ClientTickEvent.Post) {
-        TurtlShellClientEvents.TICK_POST.emit(Unit)
+        TurtlShellClientEvents.TICK_POST(Unit)
     }
 
     @SubscribeEvent
     fun onEntityLoad(e: EntityJoinLevelEvent) {
         if (e.level !is ClientLevel) return
-        TurtlShellClientEvents.ENTITY_LOAD.emit(e.entity)
+        TurtlShellClientEvents.ENTITY_LOAD(e.entity)
     }
 
     @SubscribeEvent
     fun onEntityUnload(e: EntityLeaveLevelEvent) {
         if (e.level !is ClientLevel) return
-        TurtlShellClientEvents.ENTITY_UNLOAD.emit(e.entity)
+        TurtlShellClientEvents.ENTITY_UNLOAD(e.entity)
     }
 
     @SubscribeEvent
     fun onConnect(e: ClientPlayerNetworkEvent.LoggingIn) {
-        TurtlShellClientEvents.LEVEL_CONNECTED.emit(Unit)
+        TurtlShellClientEvents.LEVEL_CONNECTED(Unit)
     }
 
     @SubscribeEvent
     fun onDisconnect(e: ClientPlayerNetworkEvent.LoggingOut) {
-        TurtlShellClientEvents.LEVEL_DISCONNECTED.emit(Unit)
+        TurtlShellClientEvents.LEVEL_DISCONNECTED(Unit)
     }
 
     @SubscribeEvent
     fun onGameStopping(e: GameShuttingDownEvent) {
-        TurtlShellClientEvents.GAME_STOPPING.emit(Unit)
+        TurtlShellClientEvents.GAME_STOPPING(Unit)
     }
 
     @SubscribeEvent
     fun onCommandSent(e: ClientChatEvent) {
         if (!e.originalMessage.startsWith("/")) return
-        TurtlShellClientEvents.COMMAND_SENT.emit(e.originalMessage.removePrefix("/"))
+        TurtlShellClientEvents.COMMAND_SENT(e.originalMessage.removePrefix("/"))
     }
 
     @SubscribeEvent
     fun onMessageReceived(e: ClientChatReceivedEvent.System) {
         if (e.isOverlay) return
-        val allowed = TurtlShellClientEvents.MESSAGE_RECEIVED.emit(e.message)
+        val allowed = TurtlShellClientEvents.MESSAGE_RECEIVED(e.message)
         if (!allowed) e.isCanceled = true
     }
 }
