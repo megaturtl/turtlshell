640e832f6e6598f35d15a7cca5ed750fae38a26b
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2026-03-18 13:41:31 +1100

    oops

--- common/src/main/kotlin/cc/turtl/turtlshell/TurtlShellCommon.kt
+++ common/src/main/kotlin/cc/turtl/turtlshell/TurtlShellCommon.kt
@@ -1,10 +1,4 @@
 package cc.turtl.turtlshell
 
-import cc.turtl.turtlshell.impl.web.WebServerManager
-
 object TurtlShellCommon {
-    const val DEFAULT_PORT = 7890
-    fun init() {
-        WebServerManager.start(DEFAULT_PORT)
-    }
 }
